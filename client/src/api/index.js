c56b2859d913339915595c6e0c3cf670ed72e66d
Kamali Sundararajan <142907186+Kamalisundar17@users.noreply.github.com>
2024-09-24 18:54:36 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url='https://sample-eight-lovat.vercel.app/';
+const url='https://sample-eight-lovat.vercel.app/posts';
 
 export const fetchPosts = ()=>  axios.get(url);
 
